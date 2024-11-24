cfe2c9e7372e1f443fa2164d412124697fe2c7ba
gregory2023 <estebancanon2023@gmail.com>
2024-11-25 00:21:19 +0100

    agrego y modifico clase Doctor

--- src/main/java/org/example/ControlCitas/personal/Doctor.java
+++ src/main/java/org/example/ControlCitas/personal/Doctor.java
@@ -2,14 +2,22 @@ package org.example.ControlCitas.personal;
 
 public class Doctor {
 
-    public int identificador;
+    public static int identificador = 0; // autoincrementado
     public String nombre;
     public String Especialidad;
 
     // comportamientos
 
+    public void doctor(){
+        System.out.println("creando objeto");
+        identificador++;
+    }
     public void imprimirNombre(){
 
         System.out.println("se llama " + nombre);
+    }
+
+    public void imprimirIdentificador(){
+        System.out.println("identificador"+identificador);
     }
 }
