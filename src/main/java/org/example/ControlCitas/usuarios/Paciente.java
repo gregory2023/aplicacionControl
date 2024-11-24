38f1b2648e83c006d5069de3950a1da5df9bcd12
gregory2023 <estebancanon2023@gmail.com>
2024-11-25 00:20:27 +0100

    agrego clase paciente

--- src/main/java/org/example/ControlCitas/usuarios/Paciente.java
+++ src/main/java/org/example/ControlCitas/usuarios/Paciente.java
@@ -1,2 +1,18 @@
-package org.example.ControlCitas.usuarios;public class Paciente {
+package org.example.ControlCitas.usuarios;
+
+public class Paciente {
+
+    public String nombre;
+    public String email;
+    String addres;
+    String numeroCelular;
+    String cumpleaños;
+    private double altura;
+    private double peso;
+    String sangre;
+
+    public void paciente (String nombre , String email  ){
+        this.nombre=nombre;
+        this.email=email;
+    }
 }
