76d0882f7f732d7f261a8279577edfffdb414897
gregory2023 <estebancanon2023@gmail.com>
2024-11-25 00:22:33 +0100

    cambio el main para generar pruebas

--- src/main/java/org/example/ControlCitas/Main.java
+++ src/main/java/org/example/ControlCitas/Main.java
@@ -1,16 +1,17 @@
 package org.example.ControlCitas;
 
 
+import org.example.ControlCitas.Menu.menu;
 import org.example.ControlCitas.personal.Doctor;
+import org.example.ControlCitas.usuarios.Paciente;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor miDoctor = new Doctor();
+        Paciente NuevoPaciente = new Paciente();
+        NuevoPaciente.nombre="gregory";
+        System.out.println(NuevoPaciente.nombre);
 
-        miDoctor.nombre = "amelie";
-        miDoctor.imprimirNombre();
     }
 }
